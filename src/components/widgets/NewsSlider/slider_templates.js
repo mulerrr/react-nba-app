f101e8b7d777ebe8a3f85ef2b992f8cb7e0a7e2c
mulerrr <muler.okeh@gmail.com>
2018-04-27 23:11:26 +0700

    6 Continue Making Slider

--- src/components/widgets/NewsSlider/slider_templates.js
+++ src/components/widgets/NewsSlider/slider_templates.js
@@ -1,13 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Slick from 'react-slick';
+import styles from './slider.css';
 
 const SliderTemplates = (props) => {
 
-    console.log(props)
+    let template = null;
+
+    const settings = {
+        dots: true,
+        infinite:  true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
+
+    switch(props.type){
+        case ('featured'):
+        template = props.data.map( (item, i) => {
+            return(
+                <div key={i}>
+                    <div className={styles.featured_item}>
+                        <div className={styles.featured_image}
+                            style={{
+                                background: `url(../images/articles/${item.image})`
+                            }}>
+                            <Link to={`/articles/${item.id}`}>
+                                <div className={styles.featured_caption}>
+                                    {item.title}
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            )
+        } )
+            break;
+        default:
+            template = null;
+    }
 
     return (
-        <div>
-            Slider
-        </div>
+        <Slick {...settings}>
+            {template}
+        </Slick>
     );
 };
 
