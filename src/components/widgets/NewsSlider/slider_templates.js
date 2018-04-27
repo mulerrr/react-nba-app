a14de708ee11d60b559dffcaf6875f5fb64dc107
mulerrr <muler.okeh@gmail.com>
2018-04-27 20:19:25 +0700

    5 Finish Footer, Setup Json Server, setup Slider

---
+++ src/components/widgets/NewsSlider/slider_templates.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SliderTemplates = (props) => {
+
+    console.log(props)
+
+    return (
+        <div>
+            Slider
+        </div>
+    );
+};
+
+export default SliderTemplates;
